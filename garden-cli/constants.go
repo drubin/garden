14f5fe251b8239acde98b8949a100806bc12e6ed
David Rubin <davidrub@gmail.com>
2018-11-06 09:44:36 +0100

    chore(build): build with goreleaser

--- garden-cli/constants.go
+++ garden-cli/constants.go
@@ -1,10 +1,15 @@
 package main
 
-// SyncImage is which docker image to use for syncing
-const SyncImage = "gardenengine/garden-sync:latest"
-
-// ServiceImage is which docker image to use for garden service
-const ServiceImage = "gardenengine/garden-service:latest"
-
-// ProjectPath is where to find the code inside ServiceImage
-const ProjectPath = "/project"
+// These need to be var so we can inject them at compile time see .circleci/config.yml
+var (
+	// SyncImage is which docker image to use for syncing
+	SyncImage = "gardenengine/garden-sync:latest"
+	// ServiceImage is which docker image to use for garden service
+	ServiceImage = "gardenengine/garden-service:latest"
+	// ProjectPath is where to find the code inside ServiceImage
+	ProjectPath = "/project"
+	// Version is the compiled version
+	Version = "0.0.1-dev-git"
+	// Commit is the commit that this was built from
+	Commit = "dirty-hash"
+)
